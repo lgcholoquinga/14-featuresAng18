f786ea9103b93aebabdd2664ce82dcb2195d0947
lgcholoquinga <luisg.choloquinga@gamil.com>
2024-08-01 21:54:14 -0500

    Change code format

--- eslint.config.js
+++ eslint.config.js
@@ -35,6 +35,8 @@ module.exports = tseslint.config(
 	{
 		files: ['**/*.html'],
 		extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
-		rules: {},
+		rules: {
+			indent: ['error', 2],
+		},
 	},
 );
