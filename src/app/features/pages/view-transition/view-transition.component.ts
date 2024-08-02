30fb79b70af06ee2e8b76aeab2a1873358432750
lgcholoquinga <luisg.choloquinga@gamil.com>
2024-08-02 12:02:02 -0500

    Learning view transitions

--- src/app/features/pages/view-transition/view-transition.component.ts
+++ src/app/features/pages/view-transition/view-transition.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TitleComponent } from '@shared/components';
 
 @Component({
 	selector: 'app-view-transition',
 	standalone: true,
-	imports: [],
+	imports: [TitleComponent],
 	templateUrl: './view-transition.component.html',
 	styles: ``,
 })
